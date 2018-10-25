18f4d44f7ad127d416c701907c9dbc7489a85751
Eric Oosterhof <eric.oosterhof@grassvalley.com>
2018-10-25 12:20:50 -0700

    [master] added constString testing

---
+++ constString/constString.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+// Or using std::cout if I only want that one specifically
+
+class test
+{
+ public:
+  const string mName;
+  test(const string &name) : mName(name) {};
+  const string name() { return mName; };
+};
+
+int main(int argc, char**argv)
+{
+  string foo("foo");
+  test t(foo);
+  test t2(string("bar"));
+  test t3("foobar");
+  
+  cout << t.name() << endl;
+  cout << t2.name() << endl;
+  cout << t3.name() << endl;
+  
+   return 0;
+}
